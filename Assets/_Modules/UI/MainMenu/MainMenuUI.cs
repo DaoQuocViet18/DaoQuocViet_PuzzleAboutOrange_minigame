f52e9ab663633a01b1a73138cf962528d0e32c87
DaoQuocViet18 <daoquocviet18062003@gmail.com>
2025-05-22 10:55:06 +0700

    MainMenu

---
+++ Assets/_Modules/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,16 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(() => {
+            Loader.Instance.LoadWithFade(SceneName.SelectLevelScene);
+            //Loader.Instance.LoadWithFade(SceneName.GameScene);
+        });
+    }
+}
